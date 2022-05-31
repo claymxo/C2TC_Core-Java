be8bb9d3f3f999b69df6744256917f0321f42b47
Yameen <Yameen@LAPTOP-O5P5IE6R>
2022-05-31 15:24:35 +0530

    first commit

---
+++ C2TC_Core-Java.git/day 03/src/Company.java
@@ -0,0 +1,11 @@
+class Company {
+	String name;
+ public static void main(String[] args) {
+	 Company c = new Company();
+	 Company c1 = new Company();
+	 c1.name = "C2TC";
+	 c.name = "Owais";
+	 System.out.println(c.name);
+	 System.out.println(c1.name);
+ }
+}
---
+++ C2TC_Core-Java.git/day 03/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float f = (1/4) * 10;
+int i = (int) f;
+System.out.println(i);
+
+	}
+
+}
